d7d64dd527905ea808ee34c0b7008fc00eda4ebd
Universal0090 <idealxorezm@gmail.com>
2022-10-26 08:17:55 -0900

    first commit

---
+++ main.js
@@ -0,0 +1,91 @@
+//misol 1
+// const dm = 100 ;
+// console.log("m= " +  dm/10);
+
+
+//misol 2
+// const kg = 2563 ;
+// console.log("t= " + kg/100);
+
+
+//misol 3 
+// let a = +prompt(' A musbat son kiritig: ') ;
+// let b = +prompt(' B musbat son kiritig: ') ; ;
+ 
+// if (a > b) {
+//    c = Math.floor(a / b)
+//     console.log(c + ' marta joylashgan'); 
+// } else if ( a < 0 || b < 0) {
+//     console.log('siz manfiy son kiritingiz'); 
+// }else if ( a < b ) {
+//     console.log('a katta bolish kerak b dan'); 
+// } else  if (a==b) {
+//     console.log('siz bir hil son yozdingiz');
+// } else {
+//     console.log('siz hato son kiritingiz');
+// }
+
+
+//misol 4
+// let ab = +prompt( 'ikki honali son kiritig: ' ) ;
+// onliklar = Math.floor( ab/10 )
+// birliklar = ab % 10  
+
+// if (ab >= 10 && ab <= 99) {
+//     console.log(onliklar + 'onliklar honasi');
+//     console.log(birliklar + 'birliklar honasi');
+// } else {
+//     console.log('siz ikki honali son kiritmadingiz !!!');
+// }
+
+
+//misol 5
+// let ab = +prompt( 'ikki honali son kiritig: ' ) ;
+// onliklar = Math.floor( ab/10 ) ;
+// birliklar = ab % 10  ;
+// yigindisi = onliklar + birliklar ;
+
+// if (ab >= 10 && ab <= 99) {
+//     console.log(yigindisi + ' = raqamlari yigindisi');
+// } else {
+//     console.log('siz ikki honali son kiritmadingiz !!!');
+// }
+
+
+//misol 6 
+// let ab = +prompt( 'ikki honali son kiritig: ' ) ;
+// onliklar = Math.floor( ab/10 ) ;
+// birliklar = ab % 10  ;
+// kopaytmasi = onliklar * birliklar ;
+
+// if (ab >= 10 && ab <= 99) {
+//     console.log(kopaytmasi + ' = raqamlari kopaytmasi');
+// } else {
+//     console.log('siz ikki honali son kiritmadingiz !!!');
+// }
+
+
+//misol 7 (tushunmadim!)
+// let abcd = +prompt( 'ikki honali son kiritig: ' ) ;
+//  abc = Math.floor(abcd / 10)
+//  a = Math.floor(abc / 100)
+
+// if (ab > 999) {
+//     console.log(a + ' = yuzlar honasi');
+// } else {
+//     console.log('siz ikki honali son kiritmadingiz !!!');
+// }
+
+
+//misol 8
+// let sek = +prompt(' sekut kiritig: ');
+// min = Math.floor( (sek / 60 ) * 100 ) / 100 
+// soat = sek / 3600
+
+// if ( sek > 0 ) {
+//     console.log(min);
+//     console.log(soat);
+// } else {
+//     console.log('siz mafiy son kiritingiz!');
+// }
+ 
