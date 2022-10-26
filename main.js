613a46ff78cd0e060984753e6cbb57a6301f441b
Universal0090 <idealxorezm@gmail.com>
2022-10-26 09:31:42 -0900

    oxirgi 16ta misol yechildi

--- main.js
+++ main.js
@@ -89,3 +89,283 @@
 //     console.log('siz mafiy son kiritingiz!');
 // }
  
+
+// 1-dars Mantiqiy amallar (if , else)
+
+//masala 1 
+// let a = +prompt( ' butun son kiritig ' ) ; 
+
+// if ( a > 0 ) {
+//     console.log('  tabriklaymiz siz musbat son kiritingiz! ');
+// } else if ( a = 0 ) {
+//     console.log(' siz nol soni kiritingiz , iltimos musbat son kiritig ');
+// } else {
+//     console.log('siz manfiy son kiritingiz , iltimos musbat son kiritig');
+// }
+
+
+//masala 2
+// let a = +prompt( ' butun son kiritig ' ) ; 
+
+// if ( a % 2 == 1 ) {
+//     console.log(' Ajoyib! bu toq son edi ');
+// } else {
+//     console.log(' bu juft son  , iltimos toq son kiritishga harakat qilig ');
+// }
+
+
+
+//masala 3
+// let a = +prompt( ' butun son kiritig ' ) ; 
+
+// if ( a % 2 == 0 ) {
+//     console.log('  Ajoyib! bu juft son edi ');
+// } else {
+//     console.log('bu toq son  , iltimos juft son kiritishga harakat qilig');
+// }
+
+
+//masala 4
+// let a = +prompt( ' A butun son kiritig ' ) ; 
+// let b = +prompt( ' B butun son kiritig ' ) ; 
+
+// if ( a > 0 && b > 0 ) {
+//     console.log('  togri');
+// } else {
+//     console.log(' hato! , iltimos boshidan miyangizni ozgina ishlatip son kiritig ');
+// }
+
+
+//masala 5
+// let a = +prompt( ' A butun son kiritig ' ) ; 
+// let b = +prompt( ' B butun son kiritig ' ) ; 
+// let c = +prompt( ' C butun son kiritig ' ) ; 
+
+// if ( a > b && b > c ) {
+//     console.log('  togri');
+// } else {
+//     console.log(' hato! , iltimos boshidan miyangizni ozgina ishlatip son kiritig ');
+// }
+
+
+
+//masala 6
+// let a = +prompt( ' A butun son kiritig ' ) ; 
+// let b = +prompt( ' B butun son kiritig ' ) ; 
+// let c = +prompt( ' C butun son kiritig ' ) ; 
+
+// if ( a <= b && b <= c ) {
+//     console.log('  togri');
+// } else {
+//     console.log(' hato! , iltimos boshidan miyangizni ozgina ishlatip son kiritig ');
+// }
+
+
+
+//masala 7
+// let a = +prompt( ' A butun son kiritig ' ) ; 
+// let b = +prompt( ' B butun son kiritig ' ) ;
+
+// if ( a % 2 == 1  ) {
+//     console.log('  togri');
+// } else {
+//     console.log(' hato! , iltimos boshidan miyangizni ozgina ishlatip son kiritig ');
+// }
+// if ( b % 2 == 1  ) {
+//     console.log('  togri');
+// } else {
+//     console.log(' hato! , iltimos boshidan miyangizni ozgina ishlatip son kiritig ');
+// }
+
+
+
+
+//masala 8
+// let a = +prompt( ' A butun son kiritig ' ) ; 
+// let b = +prompt( ' B butun son kiritig ' ) ;
+// let c = +prompt( ' C butun son kiritig ' ) ; 
+// let d = +prompt( ' D butun son kiritig ' ) ;
+
+// if ( a >= 0 && b >= 0 && c >= 0 && d >= 0 ) {
+//     console.log('  togri! .  Xammasi musbat son ');
+// } else {
+//     console.log(' hato! . orasida manfiy xam bor ');
+// }
+
+
+
+
+//masala 9
+// let a = +prompt( ' A butun son kiritig ' ) ; 
+// let b = +prompt( ' B butun son kiritig ' ) ;
+// let c = +prompt( ' C butun son kiritig ' ) ;
+
+// if ( a > 0 && b > 0 ) {
+//     console.log('  togri! .  Xammasi musbat son ');
+// } else if ( a > 0  && c > 0 ) {
+//     console.log(' togri! .  Xammasi musbat son ');
+// } else if (b > 0  && c > 0 ) {
+//     console.log(' togri! .  Xammasi musbat son ');
+// } else {
+//     console.log('xato');
+// }
+
+
+
+
+//masala 10
+// let a = +prompt( ' xafta kunin kiritig 1-7 ' ) ;
+
+// if ( a >= 1 && a <= 7 && a == 1 ) {
+//     console.log('  bugun " dushanba " ');
+// } else if ( a >= 1 && a <= 7 && a == 2 ) {
+//     console.log(' bugun " seshanba " ');
+// } else if (  a >= 1 && a <= 7 && a == 3  ) {
+//     console.log(' bugun " chorshanba " ');
+// } else if ( a >= 1 && a <= 7 && a == 4 ){
+//     console.log(' bugun " payshanba " ');
+// } else if ( a >= 1 && a <= 7 && a == 5 ){
+//     console.log(' bugun " juma " ');
+// } else if ( a >= 1 && a <= 7 && a == 6 ){
+//     console.log(' bugun " shanba " ');
+// } else if ( a >= 1 && a <= 7 && a == 7 ){
+//     console.log(' bugun " yakshanba " ');
+// } else {
+//     console.log(' aka iltimos xafta kunlarini kiritig!  ');
+// }
+
+
+
+//masala 11
+// let a = +prompt( ' telifon raqamingizni qodin kiritig:  ' ) ;
+
+// if ( 91 == a || 90 == a ) {
+//     console.log('Beeline');
+// } else if ( 99 == a ) {
+//     console.log('Uzmabile');
+// } else if ( 93 == a || 94 == a ) {
+//     console.log('Uceel');
+// } else if ( 88 == a || 87 == a ) {
+//     console.log('Mobiuz');
+// } else {
+//     console.log('bunaqa aloqa aperatori mavjud emas ! uzir');
+// }
+
+
+
+//masala 12
+// let a = +prompt( ' butun son kiritig:  ' ) ;
+// musbat = a + 1
+// manfiy = a - 1
+
+// if ( a > 0 ) {
+//     console.log(musbat);
+// } else if ( a < 0 ) {
+//     console.log(manfiy);
+// } else {
+//     console.log('bu son nolga teng');
+// }
+
+
+
+//masala 13
+// let a = +prompt( ' butun son kiritig:  ' ) ;
+// musbat = a + 3
+// manfiy = a - 2
+
+// if ( a > 0 ) {
+//     console.log(musbat);
+// } else if ( a < 0 ) {
+//     console.log(manfiy);
+// } else {
+//     console.log('bu son nolga teng');
+// }
+
+
+
+//masala 14
+// let a = +prompt( ' A son kiritig ' ) ;
+// let b = +prompt( ' B son kiritig ' )
+
+// if ( a > b ) {
+//     console.log( a );
+// } else if ( a < b ) {
+//     console.log( b );
+// } else if ( a == b ) {
+//     console.log('bu sonlar ozoro teng');
+// }
+
+
+
+//masala 15
+// let a = +prompt( ' A son kiritig ' ) ;
+// let b = +prompt( ' B son kiritig ' ) ;
+// let c = +prompt( ' C son kiritig ' ) ;
+
+// if ( a > b && b > c) {
+//     console.log( a );
+// } else if ( a > c && c > b ) {
+//     console.log( a );
+// } else if ( a > b && b >= c ) {
+//     console.log(a);
+// } else if ( b > a && a > c ) {
+//     console.log(b);
+// } else if ( b > c && c > a ) {
+//     console.log(b);
+// } else if ( b > a && a >= c ) {
+//     console.log(b);
+// } else if ( c > b && b > a ) {
+//     console.log(c);
+// } else if ( c > a && a > b ) {
+//     console.log(c);
+// } else if ( a > b && b >= a ) {
+//     console.log(c);
+// } else if ( a > b && b == c ) {
+//     console.log(a);
+// } else if ( b > a && a == c ) {
+//     console.log(b);
+// } else if ( c > b && b == a ) {
+//     console.log(c);
+// } else {
+//     console.log('bu sonlar ozoro teng');
+// }
+
+
+
+
+
+
+//misol 16 
+// let a = +prompt( ' A son kiritig ' ) ;
+// let b = +prompt( ' B son kiritig ' ) ;
+// let c = +prompt( ' C son kiritig ' ) ;
+
+// if ( a < b && b < c) {
+//     console.log( c );
+// } else if ( c > a && a > b ) {
+//     console.log(b);
+// } else if ( c > b && b > a ) {
+//     console.log(a);
+// } else if ( c > a && a >= b ) {
+//     console.log(b);
+// } else if ( c > b && b == a ) {
+//     console.log(a);
+// } else if ( b > a && a > c ) {
+//     console.log(c);
+// } else if ( b > c && c > b ) {
+//     console.log(b);
+// } else if ( b > a && a >= b ) {
+//     console.log(b);
+// } else if ( b > c && c == a ) {
+//     console.log(a);
+// } else if ( a > b && b > c ) {
+//     console.log(c);
+// } else if ( a > c && c > b ) {
+//     console.log(b);
+// } else if ( a > b && b >= c ) {
+//     console.log(c);
+// } else if ( a > c && b == c ) {
+//     console.log(c);
+// } else {
+//     console.log('bu sonlar ozoro teng');
+// }
